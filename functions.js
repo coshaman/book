fba93ee9dc4ce0036eb177f472cd0ce77cdb581a
coshaman <kmcha72@gmail.com>
2021-03-05 21:34:14 +0900

    버전1

---
+++ functions.js
@@ -0,0 +1,282 @@
+let Kore;
+
+function allkk() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	Kore = document.getElementsByClassName("li");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("el");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	return false;
+}
+
+function Koreanm() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("ko");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+function japanm() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("ja");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+function otherm() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("ot");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+function chum() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("chu");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+function fam() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("fan");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+function com() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("com");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+function rom() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("rom");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+
+function poem() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("poem");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+
+function markm() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("mar");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+
+function sptim() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("sp");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+
+function freem() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("fre");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+
+
+function godm() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("god");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+function isum() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("isu");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+function mam() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("ma");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+function artm() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("art");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+
+function hism() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("his");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+
+function donm() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("don");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+
+function shom() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("sho");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+
+function fam() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("fa");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+
+function lim() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("li");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
+
+function elm() {
+	Kore = document.getElementsByClassName("post");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "none"; 
+	}
+	Kore = document.getElementsByClassName("el");
+	for(let i = 0; i<Kore.length; i++){
+		Kore[i].style.display = "block"; 
+	}
+	return false;
+}
+
